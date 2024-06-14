d4defcd5a7fc9b5e5051a932233769b44f53695e
Alfa - Andressa Souza <45293704+alfasou@users.noreply.github.com>
2024-06-13 22:01:08 -0300

    atualização de espaços

--- aula-10/exercicio1/exercicio1_janela3.py
+++ aula-10/exercicio1/exercicio1_janela3.py
@@ -17,28 +17,28 @@ def iniciarJanela():
 
     # FORMULÁRIO
     # Nome
-    lblNome = interface.Label(frameForm, text="Nome: ", font=15)
+    lblNome = interface.Label(frameForm, text="Nome:", font=15)
     lblNome.grid(row=0, column=0, padx=30, pady=10, sticky='w')
 
     txtNome = interface.Entry(frameForm, font=15)
     txtNome.grid(row=0, column=1, padx=30, pady=10)
 
     # Email
-    lblEmail = interface.Label(frameForm, text="Email: ", font=15)
+    lblEmail = interface.Label(frameForm, text="Email:", font=15)
     lblEmail.grid(row=1, column=0, padx=30, pady=10, sticky='w')
 
     txtEmail = interface.Entry(frameForm, font=15)
     txtEmail.grid(row=1, column=1, padx=30, pady=10)
 
     # CPF
-    lblCPF = interface.Label(frameForm, text="CPF: ", font=15)
+    lblCPF = interface.Label(frameForm, text="CPF:", font=15)
     lblCPF.grid(row=2, column=0, padx=30, pady=10, sticky='w')
 
     txtCPF = interface.Entry(frameForm, font=15)
     txtCPF.grid(row=2, column=1, padx=30, pady=10)
 
     # Nome da Mãe
-    lblMae = interface.Label(frameForm, text="Nome da Mãe: ", font=15)
+    lblMae = interface.Label(frameForm, text="Nome da Mãe:", font=15)
     lblMae.grid(row=3, column=0, padx=30, pady=10, sticky='w')
 
     txtMae = interface.Entry(frameForm, font=15)
